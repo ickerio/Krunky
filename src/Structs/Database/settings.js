23e315ccb812768324ef4b6bd5730ba4e7d76a26
Harry Ickeringill <harry.ickeringill@icloud.com>
2019-06-17 12:23:54 +0800

    added validate() for settigns and prefix setting

--- src/Structs/Database/settings.js
+++ src/Structs/Database/settings.js
@@ -5,6 +5,16 @@ module.exports = [
         type: 'User',
         dbRow: 'KrunkerName',
         description: 'Link your krunker account username to skip typing it in on the `player` command',
-        valid: 'Your krunker username of length less than 15 characters'
+        valid: 'Your krunker username of length less than 15 characters',
+        validate: (parameter) => parameter.length < 15
+    },
+    {
+        usage: 'prefix',
+        displayName: 'Bot prefix',
+        type: 'Guild',
+        dbRow: 'Prefix',
+        description: 'Change Krunky\'s prefix within this server',
+        valid: 'A string of less than 5 characters with no spaces in',
+        validate: (parameter) => parameter.length < 5 && !parameter.includes(' ')
     }
 ];
