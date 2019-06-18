49bdc78f056a09c7b1249836e86be94803cb849c
Harry Ickeringill <harry.ickeringill@icloud.com>
2019-06-18 09:46:08 +0800

    quick optimize

--- src/Events/LinkDetailer.js
+++ src/Events/LinkDetailer.js
@@ -5,14 +5,15 @@ const matchmaker = new Matchmaker();
 
 async function detailer(message) {
     const reg = message.content.match(/(krunker\.io\/\?game=)([a-zA-Z]{2,3}:[a-zA-Z0-9]{5})+/);
+    if (!reg) return;
 
     const setting = message.channel.type == 'text' ?  await message.client.database.getSetting(message.guild.id) : true;
-    if (!reg || !setting) return;
+    if (!setting) return;
 
     const match = await matchmaker.getMatch(reg[2]);
 
     const desc = [
-        `Players ${match.clients}/${match.maxClients}`,
+        `Players: ${match.clients}/${match.maxClients}`,
         `Map: ${match.data.i}`,
     ].join('\n');
 
