cd25be4c04501330274f5e1df4b4e0c73f0fec4e
ickerio <harry.ickeringill@gmail.com>
2019-06-18 21:58:33 +0800

    changed for .bind() and added custom game

--- src/Events/LinkInfo.js
+++ src/Events/LinkInfo.js
@@ -1,28 +1,26 @@
-const Matchmaker = require('../Client/Matchmaker/Matchmaker.js');
 const { RichEmbed } = require('discord.js');
-
-const matchmaker = new Matchmaker();
 
 async function info(message) {
     const reg = message.content.match(/(krunker\.io\/\?game=)([a-zA-Z]{2,3}:[a-zA-Z0-9]{5})+/);
     if (!reg) return;
 
-    const setting = message.channel.type == 'text' ?  await message.client.database.getSetting(message.guild.id) : true;
+    const setting = message.channel.type == 'text' ?  await this.database.getSetting(message.guild.id, 'linkinfo') : true;
     if (!setting) return;
 
-    const match = await matchmaker.getMatch(reg[2]);
+    const match = await this.matchmaker.getMatch(reg[2]);
 
     const desc = [
         `Players: ${match.clients}/${match.maxClients}`,
         `Map: ${match.data.i}`,
+        `Custom: ${match.data.i ? 'Yes' : 'No'}`
     ].join('\n');
 
     const embed = new RichEmbed()
-        .setAuthor(`Match ${match.id}`, message.client.constants.embedImages.embedHeader, `https://${reg[1]}`)
+        .setAuthor(`Match ${match.id}`, this.constants.embedImages.embedHeader, `https://${reg[1]}`)
         .setDescription(desc)
-        .setColor(message.client.constants.embedColour);
+        .setColor(this.constants.embedColour);
 
     message.channel.send(embed);
 }
 
-module.exports = [ 'message', info ];
+module.exports = [{ name: 'message', func: info }];
